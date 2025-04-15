bd35b7a7e9140af66730ef28b1a918260658e4a2
Isaac Micheal <Mike600Isaac@gmail.com>
2025-04-15 13:18:29 +0100

    commit at intermediate level of project

--- Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/index.js
+++ Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/index.js
@@ -11,7 +11,7 @@ var buttonColour = ["green", "red", "yellow", "blue",];
 console.log(gamePattern);
 
 
-function pulse() {
+function nextSequence() {
 
     var randNum = Math.floor(Math.random() * 4);
     var randomChosenColour = buttonColour[randNum];
@@ -19,12 +19,34 @@ function pulse() {
 
     $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
-    var audio = new Audio ("./sounds/" + randomChosenColour + ".mp3");
-    audio.play();
+    // var audio = new Audio ("sounds/" + randomChosenColour + ".mp3");
+    // audio.play();
+
+    playSound(randomChosenColour);
+    animatePress(randomChosenColour);
 
 }
+var userClickedPattern =[];
 
-// function pulse() {
-//     $('.blink').fadeIn(300).fadeOut(500);
-// }
-// setInterval(pulse, 600);
+$(".btn").click(function () {
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour)
+    // console.log(userClickedPattern);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+})
+
+function playSound(name) {
+    var audio = new Audio ("sounds/" + name + ".mp3");
+    audio.play();
+}
+
+function animatePress(currentColour) {
+    $(".btn").addClass("pressed")
+}
+
+
+function animatePressRemove() {
+    $(".btn").removeClass("pressed")
+}
+setInterval(animatePressRemove, 100);
